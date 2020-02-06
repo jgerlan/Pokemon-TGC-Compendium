deb27d16c2cad7b80780c574e0054d55f082c787
José Gerlan Pereira de Paula <jose.paula@fitbank.com.br>
2020-02-06 17:07:00 -0300

    [#0001] Implementacao HtmlAgilityPack com thread

---
+++ Pokemon-TGC-Compendium/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Pokemon_TGC_Compendium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            char chooseSN = 'n';
+
+            StringBuilder sbString = new StringBuilder();
+            sbString.AppendLine("Caso de estudo - Compendio das informações simplificadas do Pokemon Trading Card Game, ");
+            sbString.AppendLine("capturado via web scraping no site do respectivo game.");
+            sbString.Append("Deseja iniciar extração dos dados? (S/N): ");
+            Console.Write(sbString);
+
+            chooseSN = char.Parse(Console.ReadLine());
+
+            if(chooseSN=='n')
+            {
+                Console.WriteLine("Obrigado!\n");
+            }
+            else
+            {
+                new RunScrapingPage();
+            }
+        }
+    }
+}
